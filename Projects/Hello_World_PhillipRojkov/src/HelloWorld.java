623f686741f781eb1c4d66fbb8e74b72a574b479
PhillipRojkov <rojkovphillip@gmail.com>
2020-09-14 13:57:40 -0700

    Added interesting fact

--- Projects/Hello_World_PhillipRojkov/src/HelloWorld.java
+++ Projects/Hello_World_PhillipRojkov/src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World"); //Print "Hello World"
+
+        System.out.println("An interesting fact about me (Phillip) is that I enjoy sailing!"); //Print interesting fact
     }
 }

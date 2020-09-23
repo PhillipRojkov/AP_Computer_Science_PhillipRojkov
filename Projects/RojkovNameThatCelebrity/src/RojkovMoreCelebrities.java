bb5a12c5019bebc1d1b48040cdd67f78c15499ed
PhillipRojkov <rojkovphillip@gmail.com>
2020-09-23 14:41:56 -0700

    added more functionality to celeb code

--- Projects/RojkovNameThatCelebrity/src/RojkovMoreCelebrities.java
+++ Projects/RojkovNameThatCelebrity/src/RojkovMoreCelebrities.java
@@ -2,7 +2,7 @@
  * Name: Rojkov Phillip
  * Date: 2020-09-23
  * Assignment Name: (0923c) Using Scanner with Strings
- * Description: Use the String and Scanner object
+ * Description: Use the String and Scanner object to ask for and shorten a celebrity name
  */
 
 import java.util.*;
@@ -12,6 +12,9 @@ public class RojkovMoreCelebrities
 {
     public static void main(String[] args)
     {
+        int charsTakeOffBeginning = 2; //Characters to take off the beginning
+        int charsTakeOffEnd = 3; //Characters to take off the end
+
         //Names
         /*
         String s1 = "Allan Alda";
@@ -26,17 +29,20 @@ public class RojkovMoreCelebrities
 
           String name = scanner.nextLine();
 
-         shortenName(name);
+         shortenName(name, charsTakeOffBeginning, charsTakeOffEnd);
         }
     }
 
-    public static void shortenName (String name) //Shortens and prints the shortened input name
+    public static void shortenName (String name, int charsTakeOffBeginning, int charsTakeOffEnd) //Shortens and prints the shortened input name
     {
-        if (name.length() <= 5) //The name shortening only works for names longer than 5 letters (otherwise error) so check here
+        if (name.length() <= (charsTakeOffBeginning + charsTakeOffEnd)) //The name shortening only works for names longer than 5 letters (otherwise error) so check here
         {
             System.out.println("That name is too short!");
         } else {
-            String shortenedName = name.substring(2, name.length() - 3); //Get the portion of the name not including first 2 and last 3 letters
+            int var1 = charsTakeOffBeginning; //Beginning of substring
+            int var2 = name.length() - charsTakeOffEnd; //End of substring
+
+            String shortenedName = name.substring(var1, var2); //Get the portion of the name not including first 2 and last 3 letters
 
             System.out.println(name + ">>>" + shortenedName); //Print shortened name
         }
--- Projects/RojkovNameThatCelebrity/src/RojkovNameThatCelebrity.java
+++ Projects/RojkovNameThatCelebrity/src/RojkovNameThatCelebrity.java
@@ -9,21 +9,27 @@ public class RojkovNameThatCelebrity
 {
     public static void main(String[] args)
     {
+        int charsTakeOffBeginning = 2; //Characters to take off the beginning
+        int charsTakeOffEnd = 3; //Characters to take off the end
+
         //Names
         String s1 = "Allan Alda";
         String s2 = "John Wayne";
         String s3 = "Gregory Peck";
         String s4 = "Justin Truedeau";
 
-        shortenName(s1);
-        shortenName(s2);
-        shortenName(s3);
-        shortenName(s4);
+        shortenName(s1, charsTakeOffBeginning, charsTakeOffEnd);
+        shortenName(s2, charsTakeOffBeginning, charsTakeOffEnd);
+        shortenName(s3, charsTakeOffBeginning, charsTakeOffEnd);
+        shortenName(s4, charsTakeOffBeginning, charsTakeOffEnd);
     }
 
-    public static void shortenName (String name) //Shortens and prints the shortened input name
+    public static void shortenName (String name, int charsTakeOffBeginning, int charsTakeOffEnd) //Shortens and prints the shortened input name
     {
-        String shortenedName = name.substring(2, name.length() - 3); //Get the portion of the name not including first 2 and last 3 letters
+        int var1 = charsTakeOffBeginning; //Beginning of substring
+        int var2 = name.length() - charsTakeOffEnd; //End of substring
+
+        String shortenedName = name.substring(var1, var2); //Get the portion of the name not including first 2 and last 3 letters
 
         System.out.println(name + ">>>" + shortenedName); //Print shortened name
     }

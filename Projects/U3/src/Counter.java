bac172c03221247b8feb33dc51142cf536a16826
PhillipRojkov <rojkovphillip@gmail.com>
2020-10-05 12:10:12 -0700

    Added count to 1000 assignment

---
+++ Projects/U3/src/Counter.java
@@ -0,0 +1,24 @@
+/*
+ * Name: Rojkov Phillip
+ * Date: 2020-10-05
+ * Assignment Name: (1005) U4A2 - Assignment 2
+ * Description: Use nested for loops to count to 1000 in 11 lines without executing a loop > 10 times
+ */
+
+public class Counter
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    System.out.print(k + (i * 100 + j * 10) + " "); //Print the number and a space
+                }
+                System.out.println(); //go down one line every 10 numbers for better readability in console
+            }
+        }
+    }
+}
